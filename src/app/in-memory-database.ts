071be1f48fde5a482acdc7a8d72bffdaecfe62b4
victorfariasdeoliveira <victorfariasdeoliveira@gmail.com>
2020-05-11 20:32:02 -0300

    Tipagem das categorias com o Model de Category ao in-memory-api

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -1,8 +1,10 @@
 import { InMemoryDbService } from 'angular-in-memory-web-api'
 
+import { Category } from './pages/categories/shared/category.model'
+
 export class InMemoryDatabase implements InMemoryDbService {
     createDb() {
-        const categories = [
+        const categories: Category[] = [
             {id: 1, name: "Lazer", description: "Cinema, parque, praia"},
             {id: 2, name: "Moradia", description: "Pagamentos de contas de casa"},
             {id: 3, name: "Saúde", description: "Plano de saúde e remédios"},
