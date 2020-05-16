bc3fbad7a40b4c28c72ffa05d0bf94287dbc849b
victorfariasdeoliveira <victorfariasdeoliveira@gmail.com>
2020-05-16 14:57:29 -0300

    Adição de módulo de formulários reativos do angular

--- src/app/pages/categories/categories.module.ts
+++ src/app/pages/categories/categories.module.ts
@@ -1,15 +1,13 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { ReactiveFormsModule } from "@angular/forms";
 
-import { CategoriesRoutingModule } from './categories-routing.module';
-import { CategoryListComponent } from './category-list/category-list.component';
-import { CategoryFormComponent } from './category-form/category-form.component';
+import { CategoriesRoutingModule } from "./categories-routing.module";
+import { CategoryListComponent } from "./category-list/category-list.component";
+import { CategoryFormComponent } from "./category-form/category-form.component";
 
 @NgModule({
-  imports: [
-    CommonModule,
-    CategoriesRoutingModule
-  ],
-  declarations: [CategoryListComponent, CategoryFormComponent]
+  imports: [CommonModule, CategoriesRoutingModule, ReactiveFormsModule],
+  declarations: [CategoryListComponent, CategoryFormComponent],
 })
-export class CategoriesModule { }
+export class CategoriesModule {}
