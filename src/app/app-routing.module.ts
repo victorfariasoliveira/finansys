b58c598a06b2684bd9f0e0108c70cf40ebeb2ce8
victorfariasdeoliveira <victorfariasdeoliveira@gmail.com>
2020-05-19 21:31:11 -0300

    Adição do loadchildren de Entries Module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,19 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
 const routes: Routes = [
-  { path: 'categories', loadChildren: './pages/categories/categories.module#CategoriesModule' }
+  {
+    path: "categories",
+    loadChildren: "./pages/categories/categories.module#CategoriesModule",
+  },
+  {
+    path: "entries",
+    loadChildren: "./pages/entries/entries.module#EntriesModule",
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
