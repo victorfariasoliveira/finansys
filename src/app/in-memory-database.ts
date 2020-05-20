ec41da3466097aa44d6faf0e2baa7a691e4c1a55
victorfariasdeoliveira <victorfariasdeoliveira@gmail.com>
2020-05-19 21:31:32 -0300

    Adição de Entries no InMemoryDatabase

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -1,6 +1,7 @@
 import { InMemoryDbService } from "angular-in-memory-web-api";
 
 import { Category } from "./pages/categories/shared/category.model";
+import { Entry } from "./pages/entries/shared/entry.model";
 
 export class InMemoryDatabase implements InMemoryDbService {
   createDb() {
@@ -12,7 +13,76 @@ export class InMemoryDatabase implements InMemoryDbService {
       { id: 5, name: "Freelas", description: "Trabalhos como freelancer" },
     ];
 
-    return { categories };
+    const entries: Entry[] = [
+      {
+        id: 1,
+        name: "Gás de cozinha",
+        categoryId: categories[0].id,
+        category: categories[0],
+        paid: true,
+        date: "14/10/2018",
+        amount: "70,80",
+        type: "expense",
+        description: "Qualquer descrição para essa despesa",
+      } as Entry,
+      {
+        id: 2,
+        name: "Remédios demais",
+        categoryId: categories[1].id,
+        category: categories[1],
+        paid: false,
+        date: "14/10/2018",
+        amount: "70,80",
+        type: "expense",
+        description: "Qualquer descrição para essa despesa",
+      } as Entry,
+      {
+        id: 3,
+        name: "Cineminha da massa",
+        categoryId: categories[2].id,
+        category: categories[2],
+        paid: true,
+        date: "14/10/2018",
+        amount: "70,80",
+        type: "expense",
+        description: "Qualquer descrição para essa despesa",
+      } as Entry,
+      {
+        id: 4,
+        name: "Ganhos demais",
+        categoryId: categories[3].id,
+        category: categories[3],
+        paid: false,
+        date: "14/10/2018",
+        amount: "70,80",
+        type: "revenue",
+        description: "Qualquer descrição para essa despesa",
+      } as Entry,
+      {
+        id: 5,
+        name: "Ganhos mais que demais",
+        categoryId: categories[4].id,
+        category: categories[4],
+        paid: true,
+        date: "14/10/2018",
+        amount: "70,80",
+        type: "revenue",
+        description: "Qualquer descrição para essa despesa",
+      } as Entry,
+      {
+        id: 6,
+        name: "Mais mais ganhos demais",
+        categoryId: categories[4].id,
+        category: categories[4],
+        paid: false,
+        date: "14/10/2018",
+        amount: "70,80",
+        type: "revenue",
+        description: "Qualquer descrição para essa despesa",
+      } as Entry,
+    ];
+
+    return { categories, entries };
   }
 }
 
