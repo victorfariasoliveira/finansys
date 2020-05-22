e0c6dc668815de31c6f44d44983c05afbde5bd85
victorfariasdeoliveira <victorfariasdeoliveira@gmail.com>
2020-05-21 22:37:24 -0300

    Adição de modules do CalendarModule e IMaskModule

--- src/app/pages/entries/entries.module.ts
+++ src/app/pages/entries/entries.module.ts
@@ -6,8 +6,17 @@ import { EntriesRoutingModule } from "./entries-routing.module";
 import { EntryListComponent } from "./entry-list/entry-list.component";
 import { EntryFormComponent } from "./entry-form/entry-form.component";
 
+import { CalendarModule } from "primeng/calendar";
+import { IMaskModule } from "angular-imask";
+
 @NgModule({
-  imports: [CommonModule, EntriesRoutingModule, ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    EntriesRoutingModule,
+    ReactiveFormsModule,
+    CalendarModule,
+    IMaskModule,
+  ],
   declarations: [EntryListComponent, EntryFormComponent],
 })
 export class EntriesModule {}
